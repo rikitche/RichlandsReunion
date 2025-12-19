129591a4a933ae40cc4b265b092d8d2b056a85b0
rikitche <148240871+rikitche@users.noreply.github.com>
2025-12-19 17:00:57 -0500

    updated student entry

--- app/directory/components/studentEntry.tsx
+++ app/directory/components/studentEntry.tsx
@@ -6,15 +6,6 @@ type StudentEntryProps = {
 export default function StudentEntry({ student }: StudentEntryProps) {
   return (
     <div className="relative group text-charcoal cursor-pointer font-serif font-semibold">
-      {/* Image */}
-      <div className="w-32 h-32 mx-auto mb-3 overflow-hidden rounded-full border-4 group-hover:border-rich-gold">
-        <img
-          src={student.imgSrc ? student.imgSrc : "/images/default-profile.png"}
-          alt={`${student.firstName} ${student.lastName}`}
-          className="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-300"
-        />
-      </div>
-
       {/* Name */}
       <span>
         {student.firstName} {student.lastName}
